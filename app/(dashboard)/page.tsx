a38696e230d79f2011578dd1982fefd1ec5028b7
servicityusa <servicityusa@gmail.com>
2024-06-01 14:34:16 -0400

    Adds dashboard cards.

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,14 +1,9 @@
-"use client";
+import DataGrid from "@/components/data-grid";
 
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  const onOpen = useNewAccount((state) => state.onOpen);
-
+export default function DashboardPage() {
   return (
-    <div>
-      <Button onClick={onOpen}>Add Account</Button>
+    <div className="max-w-scree-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid />
     </div>
   );
 }
